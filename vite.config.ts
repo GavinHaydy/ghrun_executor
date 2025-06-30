5e8964ce4c493f0f70b9a40e880634249e015121
GavinHaydy <13558622779@163.com>
2025-06-30 20:50:37 +0800

    feat: config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,46 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  server: {
+    proxy: {
+      '/permission': {
+        target: 'http://localhost:58890',
+        changeOrigin: true,
+        rewrite: path => path.replace('', '')
+      },
+      'management':{
+        target: 'http://localhost:58889',
+        changeOrigin: true,
+        rewrite: path => path.replace('', '')
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+        entryFileNames: 'assets/js[name].[hash].js',
+        chunkFileNames: 'assets/js[name].[hash].js',
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]'
+      }
+    },
+    outDir: 'webExecutor'
+  }
 })
