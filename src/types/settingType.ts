376457f1873a5ed6cef9643096bf5c9cebd1b1fe
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-07-08 23:03:39 +0800

    feat: setting type

---
+++ src/types/settingType.ts
@@ -0,0 +1,23 @@
+interface ISetting {
+    current_team_id: string;
+}
+
+interface UserInfo {
+    id: number;
+    email: string;
+    mobile: string;
+    nickname: string;
+    avatar: string;
+    role_id: string;
+    account: string;
+    role_name: string;
+    user_id: string;
+    company_id: string;
+    company_role_id: string;
+    company_role_name: string;
+}
+
+export interface ISettings {
+    settings: ISetting;
+    user_info: UserInfo;
+}
