ab561545a40918d863c9406748c032d1acf88383
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-07-09 22:52:28 +0800

    feat：setting`s api

--- src/api/setting.ts
+++ src/api/setting.ts
@@ -1,11 +1,12 @@
 import request, {Method} from "@/utils/request.ts";
 import {PREFIX} from "@/api/global.ts";
+import type {ISettings} from "@/types/settingType.ts";
 
 const API_USER_SETTING = `${PREFIX}setting/get`
 
 export const getUserSettingService = () => {
-    return request({
+    return request<ISettings>({
         url: API_USER_SETTING,
         method: Method.GET,
-    });
+    })
 }
