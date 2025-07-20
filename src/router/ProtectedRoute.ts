c89a2daed0d3c3d54e8c2c3b3c008bf9ca322bdd
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-07-20 15:03:48 +0800

    fix: get token null

--- src/router/ProtectedRoute.ts
+++ src/router/ProtectedRoute.ts
@@ -1,11 +1,10 @@
 // 路由守卫组件
-import {useAuthInfo} from "@/hooks/useAuthInfo.ts";
 import type {ReactNode} from "react";
+import Cookies from "js-cookie";
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
 
-    const {token} = useAuthInfo();
-    console.log(token)
+    const token = Cookies.get("token")
     if (!token) {
         // return <Navigate to="/login" replace />;
         window.location.href = "http://localhost:5173/login";
