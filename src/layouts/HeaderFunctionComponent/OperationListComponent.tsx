e48d5b2e652a9310f4860f513befc3bb2232b272
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-08-14 20:51:16 +0800

    feat: init Component

---
+++ src/layouts/HeaderFunctionComponent/OperationListComponent.tsx
@@ -0,0 +1,22 @@
+// import {useEffect} from "react";
+// import {Modal} from "antd";
+//
+// export interface OperationListProps {
+//     visible: boolean;
+//     teamId: string;
+// }
+//
+// export const OperationListComponent:React.FC<OperationListProps> = ({visible, teamId}) =>{
+//
+//     // const handleGetOperationList = () => {
+//     //     getOperationListService(teamId).then(r => {})
+//     // }
+//     useEffect(() => {
+//
+//     }, []);
+//     return (
+//         <Modal open={visible}>
+//             OperationListComponent
+//         </Modal>
+//     )
+// }
