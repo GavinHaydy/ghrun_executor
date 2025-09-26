842441c5991d92a5fa31613a6f0d2a3a707f944e
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-09-26 19:50:59 +0800

    feat: inputs and selects

--- src/pages/testSubject/SubjectHeaderComponent.tsx
+++ src/pages/testSubject/SubjectHeaderComponent.tsx
@@ -1,27 +1,48 @@
-import {Button, Col, Input, Row, Select} from "antd";
+import {Button, Col, Divider, Input, Row, Select} from "antd";
+import type {ITargetFolder} from "@/types/targetType.ts";
+import React from "react";
 
-export const SubjectHeaderComponent = () => {
+interface SubjectHeaderProps {
+    data: (data: ITargetFolder) => void
+}
+
+
+export const SubjectHeaderComponent: React.FC<SubjectHeaderProps> = ({data}) => {
+
+
+    const methodList = ['post', 'get', 'put', 'patch', 'delete', 'copy', 'head', 'options', 'link', 'unlink', 'purge', 'lock', 'unlock', 'propfind', 'view'];
+    const methodOpts = methodList.map(item => (
+        {
+            value: item.toUpperCase(),
+            label: item.toUpperCase()
+        }
+    ))
+
     return (
         <>
-            <Row>
-               <Col span={4}>
-                   <Input/>
-               </Col>
+            <Row style={{marginBottom: '4px'}}>
+                <Col span={4}>
+                    <Input size={'large'} value={data.name}/>
+                </Col>
                 <Col offset={16} span={2}>
-                    <Select placeholder={'请选择环境'}></Select>
+                    <Select size={'large'} placeholder={'请选择环境'}></Select>
                 </Col>
                 <Col span={2}>
-                    <Button type={"primary"} style={{width:'100%'}}>保存</Button>
+                    <Button size={'large'} type={"primary"} style={{width: '100%'}}>保存</Button>
                 </Col>
             </Row>
-            <Row>
-                <Col span={20}>
-                    <Input/>
+            <Row style={{marginBottom: '4px'}}>
+                <Col span={2}>
+                    <Select style={{width:'100%'}} size={'large'} options={methodOpts} defaultValue={'POST'}></Select>
+                </Col>
+                <Col span={18}>
+                    <Input size={'large'}/>
                 </Col>
                 <Col span={4}>
-                    <Button type={'primary'} style={{width:'100%'}}>发送并保存</Button>
+                    <Button size={'large'} type={'primary'} style={{width: '100%'}}>发送并保存</Button>
                 </Col>
             </Row>
+            <Divider size={'small'}/>
         </>
     )
 }
