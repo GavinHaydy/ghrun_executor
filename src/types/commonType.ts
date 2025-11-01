a49a4c5f82631dd10c0a6f52521fbb923c8b1398
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-01 18:41:09 +0800

    chore: common interface

--- src/types/commonType.ts
+++ src/types/commonType.ts
@@ -52,3 +52,14 @@ export interface IInviteMemberList {
     members: IInviteMember[];
 
 }
+
+export interface IParameter{
+    description?: string
+    field_type?: string
+    fileBase64?: string
+    is_checked?: number
+    key?: string
+    not_null?: number
+    type?: string
+    value?: string
+}
