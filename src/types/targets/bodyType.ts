136e0b06727a244c7b051997f8d76ca8658f162f
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-01 18:41:02 +0800

    chore: body interface

--- src/types/targets/bodyType.ts
+++ src/types/targets/bodyType.ts
@@ -1,17 +1,17 @@
+import type {IParameter} from "@/types/commonType.ts";
+
 export interface IBodyParameter{
-    description: string
-    field_type: string
+    description?: string
+    field_type?: string
     fileBase64?: string
-    id: string
-    is_checked: number
-    key: string
-    not_null: number
-    type: string
-    value: string
+    is_checked?: number
+    key?: string
+    not_null?: number
+    type?: string
+    value?: string
 }
 export interface IBodyType{
     mode: string
-    parameter: IBodyParameter
+    parameter: IParameter[]
     raw: string
-    raw_para: string[]
 }
