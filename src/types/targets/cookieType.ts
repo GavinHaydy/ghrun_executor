791fe139b4e422ec798fdf06a69cde0f0d12c8fb
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-01 18:41:16 +0800

    chore: cookie interface

--- src/types/targets/cookieType.ts
+++ src/types/targets/cookieType.ts
@@ -1,14 +1,5 @@
+import type {IParameter} from "@/types/commonType.ts";
+
 export interface ICookie {
-    description: string;
-    field_type: string;
-    fileBase64: string|null;
-    id: string;
-    is_checked: number
-    key: string;
-    not_null: number;
-    type: string;
-    value: string;
-}
-export interface ICookies {
-    parameter: ICookie[];
+    parameter: IParameter[];
 }
