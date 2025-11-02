36d01d96a4320e4bebd26ae33f2937e3e4931891
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-02 20:23:14 +0800

    feat: add change save and name function

--- src/pages/testSubject/SubjectHeaderComponent.tsx
+++ src/pages/testSubject/SubjectHeaderComponent.tsx
@@ -1,14 +1,15 @@
 import {Button, Col, Divider, Input, Row, Select} from "antd";
-import type {ITargetFolder} from "@/types/targetType.ts";
+import type { ITargetSave} from "@/types/targetType.ts";
 import React from "react";
+import {ServiceTargetSave} from "@/api/target.ts";
 
 interface SubjectHeaderProps {
-    data: ITargetFolder
+    data: ITargetSave
+    onChange: (value: ITargetSave) => void
 }
 
 
-export const SubjectHeaderComponent: React.FC<SubjectHeaderProps> = ({data}) => {
-
+export const SubjectHeaderComponent: React.FC<SubjectHeaderProps> = ({data,onChange}) => {
 
     const methodList = ['post', 'get', 'put', 'patch', 'delete', 'copy', 'head', 'options', 'link', 'unlink', 'purge', 'lock', 'unlock', 'propfind', 'view'];
     const methodOpts = methodList.map(item => (
@@ -17,18 +18,37 @@ export const SubjectHeaderComponent: React.FC<SubjectHeaderProps> = ({data}) => 
             label: item.toUpperCase()
         }
     ))
+    const handleSave = () =>{
+        ServiceTargetSave(data).then((result) => {
+            console.log(result)
+        })
+    }
+
+    const handleChangeName = (name: string) =>{
+        onChange({
+            ...data,
+            name: name
+        })
+    }
+
+    const handleChangeUrl = (url: string) =>{
+        onChange({
+            ...data,
+            url: url
+        })
+    }
 
     return (
         <>
             <Row style={{marginBottom: '4px'}}>
                 <Col span={4}>
-                    <Input size={'large'} value={data.name}/>
+                    <Input size={'large'} value={data.name} onChange={e =>handleChangeName(e.target.value)}/>
                 </Col>
                 <Col offset={16} span={2}>
                     <Select size={'large'} placeholder={'请选择环境'}></Select>
                 </Col>
                 <Col span={2}>
-                    <Button size={'large'} type={"primary"} style={{width: '100%'}}>保存</Button>
+                    <Button onClick={handleSave} size={'large'} type={"primary"} style={{width: '100%'}}>保存</Button>
                 </Col>
             </Row>
             <Row style={{marginBottom: '4px'}}>
@@ -36,7 +56,7 @@ export const SubjectHeaderComponent: React.FC<SubjectHeaderProps> = ({data}) => 
                     <Select style={{width:'100%'}} size={'large'} options={methodOpts} defaultValue={'POST'}></Select>
                 </Col>
                 <Col span={18}>
-                    <Input size={'large'}/>
+                    <Input size={'large'} value={data.url} onChange={e => handleChangeUrl(e.target.value)}/>
                 </Col>
                 <Col span={4}>
                     <Button size={'large'} type={'primary'} style={{width: '100%'}}>发送并保存</Button>
