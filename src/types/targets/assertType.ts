b180e43d1e0883086904d771db539f59c6a49d70
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-02 20:12:16 +0800

    fix: add id, index to fix assert table delete error

--- src/types/targets/assertType.ts
+++ src/types/targets/assertType.ts
@@ -1,7 +1,9 @@
 export interface IAssert{
+    // id为了表格删除
+    id?: string,
     compare: string
     is_checked: number
-    index: number
+    index?: number // 暂时没用
     response_type: number
     val: string
     var: string
