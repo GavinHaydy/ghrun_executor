a35df297b2685594ad1b3948e495dda3d457aef6
GavinHaydy <50980660+GavinHaydy@users.noreply.github.com>
2025-11-02 20:23:42 +0800

    feat: add save target api

--- src/api/target.ts
+++ src/api/target.ts
@@ -2,11 +2,20 @@ import {PREFIX} from "@/api/global.ts";
 import request, {Method} from "@/utils/request.ts";
 
 const TARGET_LIST = `${PREFIX}target/list`
+const TARGET_SAVE = `${PREFIX}target/save`
 
 export const ServiceTargetList = (data = {}) => {
   return request(
       TARGET_LIST,
       Method.GET,
+      data
+  )
+}
+
+export const ServiceTargetSave = (data = {}) => {
+  return request(
+      TARGET_SAVE,
+      Method.POST,
       data
   )
 }
